253db1b70a15d302d69aa747a0790959a7083664
Manorooban <manojmanorooban@gmail.com>
2025-03-25 02:01:12 -0700

    Create file function.js

---
+++ function.js
@@ -0,0 +1,17 @@
+//Function
+function PrintMsg(){
+	console.log("Hello Manorooban")
+}
+PrintMsg()
+
+function sum()
+{
+	return 4+6
+}
+
+console.log(sum())
+
+function sub( a, b){
+	return a-b
+}
+console.log(sub(18,10))
