d664b8c46084db62266e70ad92be6ab4bdc9af93
Manorooban <manojmanorooban@gmail.com>
2025-03-25 02:06:18 -0700

    use rest parameter function

--- arrow.js
+++ arrow.js
@@ -12,3 +12,17 @@ console.log(add(4,5))
 const mult=(a,b=2)=>{return a*b}
 console.log(mult(4,5))
 console.log(mult(4))
+
+//Rest parameter
+const mysum =(...n)=>
+{
+	 
+	console.log(n)
+}
+mysum(4,5,6,10,5,56,70,20,21,22,23)
+
+const mysum2 = (...n)=>
+{
+	return n.reduce((t,i)=>t=t+i)
+}	
+console.log(mysum2(1,2,8,9,2)) 	//print sum of total num
