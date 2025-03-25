b16aa1f60d09430ae4bab866c049d731566a1e60
Manorooban <manojmanorooban@gmail.com>
2025-03-25 02:07:11 -0700

    create a callback function

--- arrow.js
+++ arrow.js
@@ -26,3 +26,24 @@ const mysum2 = (...n)=>
 	return n.reduce((t,i)=>t=t+i)
 }	
 console.log(mysum2(1,2,8,9,2)) 	//print sum of total num
+
+
+// callback Function
+const myName = (name)=>{console.log("My name is: "+name)}
+const greet=(msg,myName)=>
+{
+	console.log("Hi.."+msg)
+}
+
+greet("Good morning",myName("David"))
+greet("Good morning",()=>{console.log("My name is Raj")})
+
+//
+const multtwo = (n)=>n*2
+
+const myarr =(mul,...n)=>
+{
+	n.forEach((i)=>console.log(mul(i)))
+}
+
+myarr(multtwo,4,5,6,8,2)	//whole num multiply into 2
