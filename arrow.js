a3c6c13e7469da689b0afb2b729ba7ff71ed6709
Manorooban <manojmanorooban@gmail.com>
2025-03-25 02:05:19 -0700

    Create a file arrow.js

---
+++ arrow.js
@@ -0,0 +1,14 @@
+//arrow Function
+
+const msg= ()=>{console.log("Hello Js")}
+	//console.log(msg)
+msg()
+
+//write  an arrow Function to sum 2 Numbes
+const add=(a,b)=>{return a+b}
+console.log(add(4,5))
+
+//default parameter
+const mult=(a,b=2)=>{return a*b}
+console.log(mult(4,5))
+console.log(mult(4))
